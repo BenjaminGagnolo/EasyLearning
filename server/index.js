9448b430cd781b62bc206b409187499ff0bea9e2
BGagnolo <benjamingagnolo@gmail.com>
2023-04-07 16:53:56 -0300

    firebase deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const server = require("./src/app.js");
+const functions = require("firebase-functions")
+const dotenv = require("dotenv")
 const { conn } = require("./src/db.js");
 const { test } = require("./src/Routes/test/controllers.js");
+
+dotenv.config()
+
 
 conn.sync({ force: true }).then(async () => {
   await test();
@@ -9,5 +14,6 @@ conn.sync({ force: true }).then(async () => {
   });
 });
 
+exports.api = functions.https.onRequest(server)
 
 
